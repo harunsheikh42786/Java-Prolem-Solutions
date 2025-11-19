5439bb5f83a4e8f9ecb6b44b260f41885c0161b7
Harun <harunsheikh42786@gmail.com>
2025-11-19 17:29:02 +0530

    solved(P1to5 : P1))

--- src/strings/P1to5.java
+++ src/strings/P1to5.java
@@ -1,4 +1,28 @@
 package strings;
 
 public class P1to5 {
+
+    public static String reverseStringUsingLoop(String str){
+        // Validate string
+        if (str == null || str.isEmpty()) throw new RuntimeException("String should not be null or empty");
+        if (str.length() == 1) return str;
+
+        StringBuilder stringBuilder = new StringBuilder(); // Doesn't create new String object in pool or Heap with new Keyword
+
+        // Reverse loop for string
+        for (int i=str.length()-1; i>=0 ; i--){
+            stringBuilder.append(str.charAt(i));
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+//        final String reversedStringUsingLoop1 = reverseStringUsingLoop(null); // throw exception
+//        final String reversedStringUsingLoop2 = reverseStringUsingLoop(""); // o/p : throw exception
+        final String reversedStringUsingLoop3 = reverseStringUsingLoop("H"); // o/p : H
+        System.out.println(reversedStringUsingLoop3);
+        final String reversedStringUsingLoop4 = reverseStringUsingLoop("Harun"); // o/p : nuraH
+        System.out.println(reversedStringUsingLoop4);
+    }
 }
