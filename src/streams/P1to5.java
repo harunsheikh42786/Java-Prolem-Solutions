7632112aadf3f2cc718d4d009e9b4c3fa84b615e
Harun <harunsheikh42786@gmail.com>
2025-11-19 18:12:21 +0530

    created(Entities package and Create Employee class)

--- src/streams/P1to5.java
+++ src/streams/P1to5.java
@@ -1,4 +1,9 @@
 package streams;
 
 public class P1to5 {
+
+    public static void main(String[] args) {
+
+    }
+
 }
---
+++ src/streams/entities/Employee.java
@@ -0,0 +1,70 @@
+package streams.entities;
+
+import java.time.LocalDate;
+
+class Employee{
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private double salary;
+    private String Department;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getDepartment() {
+        return Department;
+    }
+
+    public void setDepartment(String department) {
+        Department = department;
+    }
+
+    public Employee(String name, String email, LocalDate dob, double salary, String department) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.salary = salary;
+        Department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", salary=" + salary +
+                ", Department='" + Department + '\'' +
+                '}';
+    }
+}
